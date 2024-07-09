49a686f3ad40d3b177806061e7e39ee28fd3c04c
Maria-Vitoria815 <111195864+Maria-Vitoria815@users.noreply.github.com>
2024-07-09 12:07:33 -0300

    atu

--- Main.java
+++ Main.java
@@ -9,9 +9,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Escolha uma opção:");
-            System.out.println("1. Cadastrar Funcionario");
-            System.out.println("2. Listar Funcionarios");
+            System.out.println("Escolha a ação que deseja realizar:\n\n");
+            System.out.println("1. Cadastrar Funcionário");
+            System.out.println("2. Listar Funcionários");
             System.out.println("3. Cadastrar Empresa");
             System.out.println("4. Listar Empresas");
             int opcao = scanner.nextInt();
@@ -19,26 +19,26 @@ public class Main {
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Preencha as informações abaixo:");
+                    System.out.println("Preencha as informações abaixo:\n\n");
                     System.out.println("CPF: ");
                     String cpf = scanner.nextLine();
                     System.out.println("Nome: ");
-                    String nomeFunc = scanner.nextLine();
+                    String nomeFuncionario = scanner.nextLine();
                     System.out.println("RG: ");
                     String rg = scanner.nextLine();
                     System.out.println("Telefone: ");
                     String telefoneFuncionario = scanner.nextLine();
-                    Funcionario funcionario = new Funcionario(cpf, nomeFunc, rg, telefoneFuncionario);
+                    Funcionario funcionario = new Funcionario(cpf, nomeFuncionario, rg, telefoneFuncionario);
                     rnFuncionario.cadastrarFuncionario(funcionario);
                     break;
                 case 2:
-                    System.out.println("Funcionarios cadastrados:");
+                    System.out.println("Funcionários cadastrados:\n\n");
                     for (Funcionario f : rnFuncionario.listarFuncionario()) {
                         System.out.println(f);
                     }
                     break;
                 case 3:
-                    System.out.println("Preencha as informações abaixo:");
+                    System.out.println("Preencha as informações abaixo:\n\n");
                     System.out.println("CNPJ: ");
                     String cnpj = scanner.nextLine();
                     System.out.println("Nome: ");
@@ -49,7 +49,7 @@ public class Main {
                     rnEmpresa.cadastrarEmpresa(empresa);
                     break;
                 case 4:
-                    System.out.println("Empresas cadastradas:");
+                    System.out.println("Empresas cadastradas:\n\n");
                     for (Empresa e : rnEmpresa.listarEmpresa()) {
                         System.out.println(e);
                     }
